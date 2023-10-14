e49197c0122dee9944ed75d9e48b49ca43941ada
Lars Hoekstra <73757191+LarsGast@users.noreply.github.com>
2023-10-14 17:04:19 +0200

    #2 Extension methods for the display string for Rank en Suit enum - Create the extension methods

---
+++ DeckOfCardsLibrary/ExtensionMethods.cs
@@ -0,0 +1,48 @@
+﻿using static DeckOfCardsLibrary.Card;
+
+namespace DeckOfCardsLibrary {
+	public static class ExtensionMethods {
+
+		/// <summary>
+		/// Extension method.
+		/// Gets the value of the rank as a string for displaying.
+		/// </summary>
+		/// <param name="rank"></param>
+		/// <returns></returns>
+		public static string getDisplayString(this Rank rank) {
+			switch (rank) {
+				case Rank.Jack:
+					return "J";
+				case Rank.Queen:
+					return "Q";
+				case Rank.King:
+					return "K";
+				case Rank.Ace:
+					return "A";
+				default:
+					return ((int)rank).ToString();
+			}
+		}
+
+		/// <summary>
+		/// Extension method.
+		/// Gets the unicode value of the suit as a string for displaying.
+		/// </summary>
+		/// <param name="suit"></param>
+		/// <returns></returns>
+		public static string? getDisplayString(this Suit suit) {
+			switch (suit) {
+				case Suit.Hearts:
+					return "\u2665";
+				case Suit.Spades:
+					return "\u2660";
+				case Suit.Diamonds:
+					return "\u2666";
+				case Suit.Clubs:
+					return "\u2663";
+				default:
+					throw new ArgumentException("Undefined suit value");
+			}
+		}
+	}
+}
